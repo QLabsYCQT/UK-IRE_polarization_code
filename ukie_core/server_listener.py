8aee2706cd76e4826c68bcedde0a65dc9df7e471
Ben Amies-King <bamiesking@gmail.com>
2023-04-18 18:06:04 +0100

    feat: implemented remote function calling for instrument drivers
    wrote a class factory function that can takes an instrument driver class and wraps it in a class that handles transmitting and receiving messages via the server
    to allow code at Bob to control instruments at Alice. Also wrote a corresponding listener class with which Alice can register her instrument drivers to allow them
    to be controlled by Bob.
    
    Signed-off-by: bamiesking <bamiesking@gmail.com>

---
+++ ukie_core/server_listener.py
@@ -0,0 +1,62 @@
+from server import MessageSender
+import threading
+import json
+from time import sleep
+
+
+class FakeEpc():
+    def __init__(self):
+        pass
+
+    def setDC(self):
+        return 'successfully set to DC mode on alice'
+
+
+class ServerListener():
+
+    def __init__(self, hostname, local_name, remote_name):
+        self.hostname = hostname
+        self.local_name = local_name
+        self.remote_name = remote_name
+        self.instruments = {}
+        self.messenger = MessageSender(self.local_name, self.hostname)
+        self.retrieve_thread = threading.Thread(
+            name='retrieve',
+            target=self._retrieve_messages)
+        self.retrieve_thread.start()
+
+    def _send_message(self, message):
+        return self.messenger.send_message(self.remote_name, message)
+
+    def _retrieve_messages(self):
+        while True:
+            messages = [json.loads(message)
+                        for message in self.messenger.retrieve_messages()]
+            for message in messages:
+                print('executing message')
+                self.execute_message(message)
+                del message
+            sleep(0.01)
+
+    def execute_message(self, message: dict):
+        print(message)
+        instrument_name = message['instrument']
+        function_name = message['function']
+        args = message['args']
+        kwargs = message['kwargs']
+        instrument = self.instruments[instrument_name]
+        function = getattr(instrument, function_name)
+
+        reply = {
+            'instrument': instrument_name,
+            'function': function_name,
+            'response': function(*args, **kwargs)
+        }
+
+        print(self._send_message(json.dumps(reply)))
+
+
+if __name__ == '__main__':
+    sl = ServerListener(
+        'https://king-prawn-app-yv9q2.ondigitalocean.app', 'alice', 'bob')
+    sl.instruments['epc'] = FakeEpc()
