adfcbbab331ea65917ac1c98ba435d6d312edd30
Ben Amies-King <bamiesking@gmail.com>
2023-04-19 18:36:56 +0100

    chore: removed old form of yqcinst library
    Signed-off-by: bamiesking <bamiesking@gmail.com>
    Signed-off-by: Ben Amies-King <bamiesking@gmail.com>

--- ycqinst_keithley2231a/error_messages.py
+++
@@ -1,7 +0,0 @@
-args_msg = ('Too many arguments - for a query command, give zero arguments. '
-            'For a set command, supply the set value.')
-eight_bit_val_msg = 'The set value should be an integer from 0 to 255.'
-gpib_addr_msg = 'The set value should be an integer from 1 to 30.'
-gpib_del_msg = 'The set value should be an integer from 0 to 3.'
-ps_msg = 'The set value should be either 0 or 1.'
-pow_unit_msg = 'The set value should be either 0 or 1.'
