bd84bcca168f266a3d698d4565bf0803119a4a2d
bamiesking <bamiesking@gmail.com>
2023-04-23 22:10:30 +0100

    feat: improved config handling to always read for changes and look for a filepath in env variables
    Signed-off-by: bamiesking <bamiesking@gmail.com>
    Signed-off-by: Ben Amies-King <bamiesking@gmail.com>

--- phase_bob.py
+++ phase_bob.py
@@ -7,19 +7,26 @@ import questionary
 import nidaqmx
 import numpy as np
 import time
-
+import os
 
 RemoteEPCDriver = remote_instrument(EPCDriver, 'epc')
 RemoteKeithley = remote_instrument(Keithley2231A, 'keithley')
 
 
-def load_config(config=None):
-    with open('config.json', 'r') as f:
+def load_config():
+    config_path = os.environ.get('UKIE_CONFIG_FILE')
+    if config_path is None:
+        config_path = questionary.path(
+            'Please provide the path to the config file: ',
+            default='config.json'
+        ).ask()
+    with open(config_path, 'r') as f:
         config = json.load(f)
     return config
 
 
-def align_local(config):
+def align_local():
+    config = load_config()
     local_epc = EPCDriver(config['epc']['address'])
     print(config['po_args'])
     po = PolarisationOptimiser(**config['po_args'], epc=local_epc)
@@ -27,8 +34,9 @@ def align_local(config):
     po.gradientSearch()
 
 
-def align_remote(config):
-    remote_epc = RemoteEPCDriver(**config['server'])
+def align_remote():
+    config = load_config()
+    remote_epc = RemoteEPCDriver()
 
     remote_keithley = RemoteKeithley(**config['server'])
     remote_keithley.mode = DeviceMode.REMOTE
@@ -40,7 +48,7 @@ def align_remote(config):
     po.gradientSearch()
 
 
-def acquire_data(config):
+def acquire_data():
     with nidaqmx.Task('Read PD1') as task:
         task.ai_channels.add_ai_voltage_chan(
             'Dev1/ai0',
@@ -67,12 +75,11 @@ def acquire_data(config):
         np.save(filename, data)
 
 
-def quit(config):
+def quit():
     raise KeyboardInterrupt
 
 
 processes = {
-    'load config': load_config,
     'local EPC alignment': align_local,
     'remote EPC alignment': align_remote,
     'data acquisition': acquire_data,
@@ -85,12 +92,11 @@ menu = questionary.select(
     choices=list(processes.keys())
 )
 
-config = load_config()
 try:
     while True:
         process = menu.ask()
         try:
-            processes[process](config)
+            processes[process]()
         except Exception as e:
             print(f'An exception occured whilst executing process {process}:')
             print(f'{repr(e)}')
