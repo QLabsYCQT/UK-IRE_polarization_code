48f033ff97df8f5a3db5d768049ff8625d14e13f
bamiesking <bamiesking@gmail.com>
2023-04-26 18:06:54 +0100

    feat: added a process for monitoring detector output in real time
    Signed-off-by: Ben Amies-King <bamiesking@gmail.com>

--- phase_bob.py
+++ phase_bob.py
@@ -8,6 +8,8 @@ import nidaqmx
 import numpy as np
 import time
 import os
+import matplotlib.pyplot as plt
+from matplotlib.lines import Line2D
 
 RemoteEPCDriver = remote_instrument(EPCDriver, 'epc')
 RemoteKeithley = remote_instrument(Keithley2231A, 'keithley')
@@ -47,6 +49,62 @@ def align_remote():
     po.gradientSearch()
 
 
+def monitor_power():
+    config = load_config()
+    remote_keithley = RemoteKeithley(**config['server'])
+    config = config['acquisition']
+    remote_keithley.mode = DeviceMode.REMOTE
+    remote_keithley.enabled = [True, True, True]
+    remote_keithley.voltage = config['EVOA voltages']
+    time.sleep(1)
+    voltage_min, voltage_max = config['voltage limits']
+
+    fig, ax = plt.subplots(1, 1)
+    ai0 = Line2D([], [], color='blue', label='ai0')
+    ai1 = Line2D([], [], color='orange', label='ai1')
+    ax.add_line(ai0)
+    ax.add_line(ai1)
+    ax.set_xlabel('time / s')
+    ax.set_ylabel('voltage / V')
+    ax.set_xlim(0, 10)
+    ax.set_ylim(voltage_min, voltage_max)
+    ax.legend(loc='upper right')
+
+    with nidaqmx.Task('Read PD1') as task:
+        task.ai_channels.add_ai_voltage_chan(
+            'Dev1/ai0',
+            min_val=voltage_min,
+            max_val=voltage_max)
+        task.ai_channels.add_ai_voltage_chan(
+            'Dev1/ai1',
+            min_val=voltage_min,
+            max_val=voltage_max)
+        task.timing.cfg_samp_clk_timing(
+            2_000,
+            sample_mode=nidaqmx.constants.AcquisitionType.CONTINUOUS)
+        stream = task.in_stream
+        reader = nidaqmx.stream_readers.AnalogMultiChannelReader(stream)
+        task.start()
+        data = np.zeros((2, 200))
+        history = np.zeros((2, 20_000))
+        t = np.linspace(0, 10, 20_000)
+        fig.canvas.draw()
+        try:
+            while True:
+                reader.read_many_sample(
+                    data,
+                    200,
+                    timeout=nidaqmx.constants.WAIT_INFINITELY)
+                history = np.roll(history, 200, axis=1)
+                history[:,:200] = data
+                ai0.set_data(t, history[0])
+                ai1.set_data(t, history[1])
+                fig.canvas.draw()
+                plt.pause(0.01)
+        except KeyboardInterrupt:
+            task.stop()
+
+
 def acquire_data():
     with nidaqmx.Task('Read PD1') as task:
         task.ai_channels.add_ai_voltage_chan(
@@ -81,6 +139,7 @@ def quit():
 processes = {
     'local EPC alignment': align_local,
     'remote EPC alignment': align_remote,
+    'monitor detectors': monitor_power,
     'data acquisition': acquire_data,
     'quit': quit
 }
